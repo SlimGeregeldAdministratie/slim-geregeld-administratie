f9bf742735e5988f20b11b5ae20dae813878ebd8
SlimGeregeldAdministratie <kyle@slim-geregeld.nl>
2026-01-05 18:17:21 +0100

    Update Nieuws.tsx met nieuwe LinkedIn post: Pensioenbeleggen: fiscaal voordeel dat veel zzp'ers missen

--- src/components/Nieuws.tsx
+++ src/components/Nieuws.tsx
@@ -22,12 +22,12 @@ const Nieuws = () => {
 
   const linkedInPosts = [
     {
+      url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7413991999038722048?collapsed=1',
+      title: 'Pensioenbeleggen: fiscaal voordeel dat veel zzp\'ers missen'
+    },
+{
       url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7405274545223323648?collapsed=1',
       title: 'Belastingwijzigingen 2026: wat betekent het voor jou als zzp\'er?'
-    },
-    {
-      url: 'https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7390822552698310656?collapsed=1',
-      title: 'Technologie staat niet stil, ook niet in de boekhouding. 🤖'
     }
   ];
   
