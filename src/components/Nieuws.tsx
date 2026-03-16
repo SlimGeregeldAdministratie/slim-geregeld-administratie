f95b898769607e5549a7ecfd03710c7a8bd391ff
SlimGeregeldAdministratie <kyle@slim-geregeld.nl>
2026-03-16 18:16:54 +0100

    Update Nieuws.tsx met nieuwe LinkedIn post: Overstappen naar een BV? Niet altijd de beste keuze

--- src/components/Nieuws.tsx
+++ src/components/Nieuws.tsx
@@ -22,12 +22,12 @@ const Nieuws = () => {
 
   const linkedInPosts = [
     {
-      url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7437121541320028160?collapsed=1',
-      title: 'Pensioen opbouwen? Het is echt geen optie maar een must.'
+      url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7439359043900657665?collapsed=1',
+      title: 'Overstappen naar een BV? Niet altijd de beste keuze'
     },
 {
-      url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7434369881468272640?collapsed=1',
-      title: 'Bescherm en groei je vermogen met slimme stappen'
+      url: 'https://www.linkedin.com/embed/feed/update/urn:li:share:7437121541320028160?collapsed=1',
+      title: 'Pensioen opbouwen? Het is echt geen optie maar een must.'
     }
   ];
   
